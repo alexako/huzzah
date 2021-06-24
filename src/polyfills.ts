749c08067ebda9454e32b6ef85e4657d1c357d1f
kludgebot <akosialexpo@gmail.com>
2021-06-23 23:13:50 -0700

    add: Polyfills for IE11 support

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -54,6 +54,14 @@
  *
  */
 
+import 'core-js/es/typed-array';
+import 'core-js/es/object';
+
+(window as any).global = window;
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
+
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
@@ -63,3 +71,4 @@ import 'zone.js';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
